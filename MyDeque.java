0e2c5f3aa9dcf0f4f71369a27bbc2d89561f580d
Jessica Kim <jkim00@stuy.edu>
2019-03-21 11:46:10 -0400

    removeFirst, removeLast, getFirst, getLast, addFirst, and addLast all written WITHOUT considering edge cases, always assuming there's enough space

--- MyDeque.java
+++ MyDeque.java
@@ -2,14 +2,54 @@ public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
-  public MyDeque(){  }
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
-  public void addFirst(E element){ }
-  public void addLast(E element){ }
-  public E removeFirst(){ }
-  public E removeLast(){ }
-  public E getFirst(){ }
-  public E getLast(){ }
+  @SuppressWarnings("unchecked")
+  public MyDeque(){
+    data = (E[])new Object[10];
+  }
+
+  public MyDeque(int initialCapacity){
+
+  }
+
+  public int size(){
+    return -1;
+  }
+
+  public String toString(){
+    String s = "";
+    return s;
+  }
+
+  public void addFirst(E element){
+    //assuming there's more space in the beginning
+    data[start-1] = element;
+    start--;
+  }
+
+  public void addLast(E element){
+    //assuming there's more space in the end
+    data[end+1] = element;
+    end++;
+  }
+
+  public E removeFirst(){
+    E first = data[start];
+    start++;
+    return first;
+  }
+
+  public E removeLast(){
+    E last = data[end];
+    end--;
+    return last;
+  }
+
+  public E getFirst(){
+    return data[start];
+  }
+
+  public E getLast(){
+    return data[end];
+  }
+
 }
